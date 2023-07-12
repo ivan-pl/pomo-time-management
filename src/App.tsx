cb421d3e71d6315998840e22bce380878b9860c2
ivan-pl <pleskaiv@gmail.com>
2023-07-12 11:25:00 +0300

    feat: add basic login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { Routes, Route } from "react-router-dom";
 
 import Error404 from "./pages/404";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export const App: FC = () => (
   <Routes>
-    <Route path="login" element={<div>Auth page</div>} />
+    <Route path="login" element={<Login />} />
     <Route path="/" element={<Home />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
