fd5de0d7f8f95743d5352b9f584c37b24d63444d
ivan-pl <pleskaiv@gmail.com>
2023-07-13 17:55:42 +0300

    feat: implement logout

--- src/pages/Home/MenuBar.tsx
+++ src/pages/Home/MenuBar.tsx
@@ -3,15 +3,25 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import AlarmOnIcon from "@mui/icons-material/AlarmOn";
 import LogoutIcon from "@mui/icons-material/Logout";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AssessmentIcon from "@mui/icons-material/Assessment";
+import { useNavigate } from "react-router-dom";
+
 import ResponsiveIconButton from "../../components/ResponsiveIconButton";
+import { useAppDispatch } from "../../app/hooks";
+import { clearAuthCredentials } from "../Login/authSlice";
 
 const MenuBar: FC = () => {
   const [openSettings, setOpenSettings] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(clearAuthCredentials());
+    navigate("./login");
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -39,6 +49,7 @@ const MenuBar: FC = () => {
             color="inherit"
             sx={{ margin: "0 6px" }}
             startIcon={<LogoutIcon />}
+            onClick={handleLogout}
           >
             Logout
           </ResponsiveIconButton>
